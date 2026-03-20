3571002d8cab30ca3b3c71a422e13119a904131c
t1end4t <tiendatarse3004@gmail.com>
2026-03-20 10:09:29 +0700

    update template for handle env

--- python/flake.nix
+++ python/flake.nix
@@ -113,22 +113,22 @@
                   };
                 };
 
-                scripts.init-project.exec = ''
-                  ${pkgs.uv}/bin/uv init
-                  ${pkgs.uv}/bin/uv sync
-                  ${pkgs.uv}/bin/uv add --dev -r dev-requirements.txt
-                '';
+                # scripts.init-project.exec = ''
+                #   ${pkgs.uv}/bin/uv init
+                #   ${pkgs.uv}/bin/uv sync
+                #   ${pkgs.uv}/bin/uv add --dev -r dev-requirements.txt
+                # '';
 
                 enterShell = ''
                   source .devenv/state/venv/bin/activate
                 '';
 
-                env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-                  pkgs.stdenv.cc.cc
-                  pkgs.zlib
-                  pkgs.libGL
-                  pkgs.glib
-                ];
+                # env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+                #   pkgs.stdenv.cc.cc
+                #   pkgs.zlib
+                #   pkgs.libGL
+                #   pkgs.glib
+                # ];
               }
             ];
           };
